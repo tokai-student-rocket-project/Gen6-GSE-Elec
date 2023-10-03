9d0f4ee034770dd20ac544db335bc7cd00de2477
Yuki Iwai <waiwai.cps@gmail.com>
2023-10-03 15:20:10 +0900

    インクルードガードとインクルードの改行を削除

--- Sources/LaunchController/LaunchController.ino
+++ Sources/LaunchController/LaunchController.ino
@@ -2,33 +2,38 @@
 #include "Control.hpp"
 #include "AccessLED.hpp"
 #include "Button.hpp"
+#include "AudioPlayer.hpp"
 
 
 namespace control {
-  Control* power = new Control(A5);
+  Control power(A5);
 }
 
 namespace LED {
-  AccessLED* task = new AccessLED(A12);
+  AccessLED task(A12);
 }
 
 namespace button {
-  Button* kill = new Button(14, false);
+  Button kill(14, false);
+}
+
+namespace device {
+  // AudioPlayer audioPlayer;
 }
 
 
 void setup() {
-  control::power->turnOn();
+  control::power.turnOn();
 }
 
 
 void loop() {
-  if (button::kill->isPushed()) {
+  if (button::kill.isPushed()) {
     // 終了処理
-    control::power->turnOff();
+    control::power.turnOff();
   }
 
   // HACK TaskManagerを追加していないのでloop内で処理
-  LED::task->blink();
+  LED::task.blink();
   delay(100);
 }
