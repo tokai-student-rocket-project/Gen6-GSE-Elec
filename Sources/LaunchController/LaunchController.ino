fdb7e270dc9450c6d2b84535fdd1abc8e2751fd8
Yuki Iwai <waiwai.cps@gmail.com>
2023-10-04 13:41:17 +0900

    AVR pinoutに変更

--- Sources/LaunchController/LaunchController.ino
+++ Sources/LaunchController/LaunchController.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <TaskManager.h>
 #include <DFPlayer_Mini_Mp3.h>
 #include "Control.hpp"
 #include "AccessLED.hpp"
@@ -6,15 +7,24 @@
 
 
 namespace control {
-  Control power(A5);
+  Control power(PIN_PF5);
 }
 
-namespace LED {
-  AccessLED task(A12);
+namespace indicator {
+  AccessLED task(PIN_PK4);
+
+  // LED
+  Control emergencyStop(PIN_PG4);
+  Control dump(PIN_PB5);
 }
 
 namespace button {
-  Button kill(14, false);
+  Button kill(PIN_PJ1, false);
+  Button emergencyStop(PIN_PC4, false);
+}
+
+namespace task {
+  void handleManualControl();
 }
 
 
@@ -31,16 +41,32 @@ void setup() {
   // mp3_set_debug_serial(Serial);
   // mp3_set_volume(30);
   // mp3_play(1);
+
+  Tasks.add(&task::handleManualControl)->startFps(20);
+
+  // Tasks.add<EmergencyStop>("EmergencyStop")
+  //   ->sync<EmergencyStop>("Dump", [&](TaskRef<EmergencyStop> task) {
+  //   task->dump();
+  //     });
 }
 
 
 void loop() {
+  Tasks.update();
+}
+
+
+void task::handleManualControl() {
   if (button::kill.isPushed()) {
     // 終了処理
     control::power.turnOff();
   }
 
-  // HACK TaskManagerを追加していないのでloop内で処理
-  LED::task.blink();
-  delay(100);
+  // HACK 仮エマスト
+  if (button::emergencyStop.isPushed()) {
+    indicator::emergencyStop.turnOn();
+    indicator::dump.turnOn();
+  }
+
+  indicator::task.blink();
 }
