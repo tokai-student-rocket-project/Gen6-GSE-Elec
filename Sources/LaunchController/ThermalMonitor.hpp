9f73f02f12c2797549dc98be030e6aeffab87828
Yuki Iwai <waiwai.cps@gmail.com>
2023-10-05 19:15:24 +0900

    サーミスタの固有値を定数化

--- Sources/LaunchController/ThermalMonitor.hpp
+++ Sources/LaunchController/ThermalMonitor.hpp
@@ -7,6 +7,10 @@ class ThermalMonitor {
 
   float _lowerResistance;
 
+  const float T0 = 25.0 + 273.15;
+  const float R0 = 10000.0;
+  const float B = 3380.0;
+
 public:
   ThermalMonitor(uint8_t pinNumber, float lowerResistance);
 
