ca8547120b89f210f30ab617fae6694a41efb598
HirokiTsutsumi <hiroki1815@outlook.jp>
2026-02-17 00:50:42 +0900

    HSV機能を追加

--- lib/Lib_Neopixel/Lib_Neopixel.hpp
+++ lib/Lib_Neopixel/Lib_Neopixel.hpp
@@ -1,24 +1,25 @@
 #pragma once
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 
-class Neopixel
-{
-    Adafruit_NeoPixel *_neopixel;
+
+class Neopixel {
+  Adafruit_NeoPixel *_neopixel;
 
 public:
-    Neopixel(uint8_t pin);
-    void init(uint8_t power);
-    void off();
-    void noticedPink();
-    void noticedGreen();
-    void noticedRed();
-    void noticedBlue();
-    void noticedWhite();
-    void noticedTime(bool permiddionTime);
-    void noticedRainbow();
-    void setBatteryStatus(float voltage);
+  Neopixel(uint8_t pin);
+  void init(uint8_t power);
+  void off();
+  void noticedPink();
+  void noticedGreen();
+  void noticedRed();
+  void noticedBlue();
+  void noticedWhite();
+  void noticedTime(bool permiddionTime);
+  void setHSV(uint16_t hue, uint8_t sat, uint8_t val);
+  void show();
+  void setBatteryStatus(float voltage);
 
 private:
-    uint8_t _power;
+  uint8_t _power;
 };
